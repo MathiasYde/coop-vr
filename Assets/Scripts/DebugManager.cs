70353f1db041edd2918c3536d4d4ef16e673f231
Mathias <mathias.yde@hotmail.dk>
2023-02-14 14:36:04 +0100

    Implement DebugManager for easier debugging in Unity Events

---
+++ Assets/Scripts/DebugManager.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour {
+
+    public void Log(String message) {
+        Debug.Log(message);
+    }
+}
