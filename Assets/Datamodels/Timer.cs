ef04734d57101818258fdb0c66dd98bbfcd1c225
Mathias <mathias.yde@hotmail.dk>
2023-02-17 11:29:22 +0100

    Import Timer datamodel

---
+++ Assets/Datamodels/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Timer {
+    private float timer;
+    [SerializeField] private float timeout;
+
+    public event Action onTimerEnd;
+
+    public bool isFinished => timer >= timeout;
+    public float remainingTime => timeout - timer;
+
+    public Timer() { }
+    public Timer(float time) {
+        this.timeout = time;
+    }
+
+    public float GetTimeout() {
+        return timeout;
+    }
+
+    public void SetTimeout(float timeout) {
+        this.timeout = timeout;
+    }
+
+    public void Update(float deltaTime) {
+        timer += deltaTime;
+        if (isFinished) {
+            onTimerEnd?.Invoke();
+        }
+    }
+
+    public void Reset() {
+        timer = 0.0f;
+    }
+}
---
+++ Assets/Editor/TimerPropertyDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    [CustomPropertyDrawer(typeof(Timer))]
+    public class TimerPropertyDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            SerializedProperty timeoutProperty = property.FindPropertyRelative("timeout");
+
+            EditorGUI.PropertyField(position, timeoutProperty, label);
+        }
+    }
+}
