1a8b74bb2a3ec87430e821ec2958e8107a3dee22
Mathias <mathias.yde@hotmail.dk>
2023-03-24 10:56:50 +0100

    Update Message to include helper messages retrieving values from payload

--- Assets/MessageSystem/Datamodels/Message.cs
+++ Assets/MessageSystem/Datamodels/Message.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace MessageSystem {
 	public class Message {
 		public readonly string topic;
 		public readonly Dictionary<string, object> payload;
+
+		public string GetString(string key) { return Convert.ToString(payload[key]); }
+		public int GetInt(string key) { return Convert.ToInt32(payload[key]); }
+		public float GetFloat(string key) { return Convert.ToSingle(payload[key]); }
+
+		public Vector3 GetVector3(string xKey, string yKey, string zKey) {
+			return new Vector3(
+				GetFloat(xKey),
+				GetFloat(yKey),
+				GetFloat(zKey));
+		}
 
 		public Message(string topic, Dictionary<string, object> payload) {
 			this.topic = topic;
