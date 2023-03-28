7722664f09419a1ac256f6c0ddc11a0b161b84cc
Mathias <mathias.yde@hotmail.dk>
2023-03-28 15:00:30 +0200

    Add utils script

---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Utils {
+	public static bool TryRaycast(Vector3 origin, Vector3 direction, out RaycastHit hit, float distance,
+		LayerMask layerMask) {
+		Debug.DrawRay(origin, direction * distance);
+		return Physics.Raycast(origin, direction, out hit, distance, layerMask);
+	}
+}
