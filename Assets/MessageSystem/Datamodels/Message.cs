fa4884b2dac81cf12bd4bbc658f069f4abc7ef4e
Mathias <mathias.yde@hotmail.dk>
2023-04-13 15:18:13 +0200

    Update Message class to use expresion bodies

--- Assets/MessageSystem/Datamodels/Message.cs
+++ Assets/MessageSystem/Datamodels/Message.cs
@@ -7,9 +7,10 @@ namespace MessageSystem {
 		public readonly string topic;
 		public readonly Dictionary<string, object> payload;
 
-		public string GetString(string key) { return Convert.ToString(payload[key]); }
-		public int GetInt(string key) { return Convert.ToInt32(payload[key]); }
-		public float GetFloat(string key) { return Convert.ToSingle(payload[key]); }
+		public string GetString(string key) => Convert.ToString(payload[key]);
+		public int GetInt(string key) => Convert.ToInt32(payload[key]);
+		public float GetFloat(string key) => Convert.ToSingle(payload[key]);
+		public bool GetBool(string key) => Convert.ToBoolean(payload[key]);
 
 		public Vector3 GetVector3(string xKey, string yKey, string zKey) {
 			return new Vector3(
@@ -17,7 +18,7 @@ namespace MessageSystem {
 				GetFloat(yKey),
 				GetFloat(zKey));
 		}
-
+ 
 		public Message(string topic, Dictionary<string, object> payload) {
 			this.topic = topic;
 			this.payload = payload;
