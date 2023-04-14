9b7eef21e10f3d3b591e1b3b6f9c15d53fabef2d
Mathias <mathias.yde@hotmail.dk>
2023-04-14 15:06:26 +0200

    Update Flask

--- Assets/Scripts/Flask.cs
+++ Assets/Scripts/Flask.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class Flask : MonoBehaviour {
 	[SerializeField] private FlaskColor color;
+
+	[Header("Actions")]
+	[SerializeField] private SteamVR_Action_Boolean pickupAction;
 	
 	[Header("Components")]
 	[SerializeField] private Renderer liquidRenderer;
+	[SerializeField] private Interactable interactable;
+	
+	private bool isHovering = false;
 
 	private void Awake() {
 		Debug.Assert(liquidRenderer != null, "Flask.liquidRenderer is null");
@@ -13,5 +20,13 @@ public class Flask : MonoBehaviour {
 
 	private void Start() {
 		liquidRenderer.material.SetColor(MaterialKeywords.Color, color.ToColor() ?? Color.black);
+	}
+	
+	private void OnHandHoverBegin() {
+		isHovering = true;
+	}
+
+	private void OnHandHoverEnd() {
+		isHovering = false;
 	}
 }
