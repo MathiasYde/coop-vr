c12fe4d6d33f55a378616d8b0c3fd982b18075d5
Mathias <mathias.yde@hotmail.dk>
2023-04-14 12:24:57 +0200

    Implement flask

--- Assets/Data/MaterialKeywords.cs
+++ Assets/Data/MaterialKeywords.cs
@@ -1,5 +1,7 @@
 ﻿public static class MaterialKeywords {
 	public static readonly string Emission = "_EMISSION";
 
+	public static readonly string Color = "_Color";
+
 	public static readonly string EmissionColor = "_EmissionColor";
 }
---
+++ Assets/Scripts/Flask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class Flask : MonoBehaviour {
+	[SerializeField] private FlaskColor color;
+	
+	[Header("Components")]
+	[SerializeField] private Renderer liquidRenderer;
+
+	private void Awake() {
+		Debug.Assert(liquidRenderer != null, "Flask.liquidRenderer is null");
+	}
+
+	private void Start() {
+		liquidRenderer.material.SetColor(MaterialKeywords.Color, color.ToColor() ?? Color.black);
+	}
+}
