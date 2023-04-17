48d8c4dffc59d4d1723299e6472412cbdd8e8d62
Mathias <mathias.yde@hotmail.dk>
2023-04-17 11:57:29 +0200

    Update Extensions.cs

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -37,4 +37,13 @@ public static class Extensions {
 			default: return null;
 		}
 	}
+	
+	public static Vector2Int ToVector2Int(this Vector2 vector) => new Vector2Int(Mathf.RoundToInt(vector.x), Mathf.RoundToInt(vector.y));
+	
+	public static Vector2 xy(this Vector3 vector) => new Vector2(vector.x, vector.y);
+	public static Vector2 xz(this Vector3 vector) => new Vector2(vector.x, vector.z);
+	public static Vector2 yx(this Vector3 vector) => new Vector2(vector.y, vector.x);
+	public static Vector2 yz(this Vector3 vector) => new Vector2(vector.y, vector.z);
+	public static Vector2 zx(this Vector3 vector) => new Vector2(vector.z, vector.x);
+	public static Vector2 zy(this Vector3 vector) => new Vector2(vector.z, vector.y);
 }
